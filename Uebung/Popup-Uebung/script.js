3596752ec2e8024b0f4972f443cc10cb5b40f911
Gwengal <alexanderkaiser93@yahoo.com>
2024-05-22 08:06:21 +0200

    Popup Uebungen

---
+++ Uebung/Popup-Uebung/script.js
@@ -0,0 +1,9 @@
+function openPopup() {
+    const popup = document.getElementById("popup");
+    popup.classList.add("open");
+}
+
+function closePopup() {
+    const popup = document.getElementById("popup");
+    popup.classList.remove("open");
+}
